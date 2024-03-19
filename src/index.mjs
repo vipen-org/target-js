98c217825b467edbb73d1aa5a2fd7cce3332b542
eXory2024 <exory@anio.software>
2024-03-19 01:24:22 +0100

    Fix initializeTarget().

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,6 @@
 import initLibraryProject from "./type/library/init.mjs"
 
-export async function initializeTarget() {
+export async function initializeTarget(context) {
 	switch (context.type) {
 		case "library": {
 			await initLibraryProject(context)
