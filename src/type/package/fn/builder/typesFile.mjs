35d2a1b2138c124fd1c2c1ece95f167c8e25b47f
eXory2024 <exory@anio.software>
2024-04-04 04:16:50 +0200

    Remove minified option.

--- src/type/package/fn/builder/typesFile.mjs
+++ src/type/package/fn/builder/typesFile.mjs
@@ -3,8 +3,7 @@ import runBundler from "./../../../../fn/bundler/index.mjs"
 export default async function(vipen_session) {
 	return await runBundler(
 		vipen_session, {
-			entry: "src/index.d.ts",
-			minified: false
+			entry: "src/index.d.ts"
 		}
 	)
 }
