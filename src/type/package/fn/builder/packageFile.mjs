7812844d3e51c735bb74040c83ad5de94ac19395
eXory2024 <exory@anio.software>
2024-04-05 07:04:02 +0200

    Add experimental support for additional entry points specified by target.additional_entry_points.

--- src/type/package/fn/builder/packageFile.mjs
+++ src/type/package/fn/builder/packageFile.mjs
@@ -1,9 +1,9 @@
 import runBundler from "./../../../../fn/bundler/index.mjs"
 
-export default async function(vipen_session, file_path, minified = false) {
+export default async function(vipen_session, file_path, entry_point, minified = false) {
 	return await runBundler(
 		vipen_session, {
-			entry: "src/index.mjs",
+			entry: entry_point,
 			minified
 		}
 	)
--- src/type/package/init.mjs
+++ src/type/package/init.mjs
@@ -3,16 +3,40 @@ import buildTypesFile from "./fn/builder/typesFile.mjs"
 import path from "node:path"
 import {isRegularFile} from "@anio-node-foundation/fs-utils"
 
+async function handleAdditionalEntryPoints(vipen_session, entry_points) {
+	for (const output in entry_points) {
+		const entry_point = entry_points[output]
+
+		vipen_session.distributables.addFile(`additional_entry_points/${output}/package.mjs`, {
+			generator: buildPackageFile,
+			generator_args: [entry_point, false]
+		})
+
+		vipen_session.distributables.addFile(`additional_entry_points/${output}/package.min.mjs`, {
+			generator: buildPackageFile,
+			generator_args: [entry_point, true]
+		})
+	}
+}
+
 export default async function(vipen_session) {
+	const project_config = vipen_session.getProjectConfig()
+
 	vipen_session.distributables.addFile(`package.mjs`, {
 		generator: buildPackageFile,
-		generator_args: [false]
+		generator_args: ["src/index.mjs", false]
 	})
 
 	vipen_session.distributables.addFile(`package.min.mjs`, {
 		generator: buildPackageFile,
-		generator_args: [true]
+		generator_args: ["src/index.mjs", true]
 	})
+
+	const additional_entry_points = project_config?.target?.additional_entry_points
+
+	if (additional_entry_points) {
+		await handleAdditionalEntryPoints(vipen_session, additional_entry_points)
+	}
 
 	const types_path = path.join(vipen_session.getProjectRoot(), "src", "index.d.ts")
 
